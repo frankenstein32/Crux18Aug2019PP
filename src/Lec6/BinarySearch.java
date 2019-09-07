10e63f328cc0a6ab526cdda769bf9ccb73c6e1e1
Rashid Saifi <saifirashid32@gmail.com>
2019-09-07 17:05:40 +0530

    Lec6 Codes

---
+++ src/Lec6/BinarySearch.java
@@ -0,0 +1,36 @@
+package Lec6;
+
+public class BinarySearch {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		int[] arr = { 10, 20, 30, 40, 50, 60 };
+		int item = 60;
+		System.out.println(search(arr, item));
+	}
+
+	public static int search(int[] arr, int item) {
+
+		int lo = 0, hi = arr.length - 1;
+
+		while (lo <= hi) {
+
+			int mid = (lo + hi) / 2;
+
+			if (item < arr[mid]) {
+
+				hi = mid - 1;
+			} else if (item > arr[mid]) {
+				lo = mid + 1;
+			} else {
+				return mid;
+			}
+
+		}
+
+		return -1;
+
+	}
+
+}
---
+++ src/Lec6/BubbleSort.java
@@ -0,0 +1,35 @@
+package Lec6;
+
+public class BubbleSort {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		int[] arr = { 5, 4, 3, 2, 1 };
+		sort(arr);
+
+		for (int val : arr) {
+			System.out.print(val + " ");
+		}
+
+	}
+
+	public static void sort(int[] arr) {
+
+		for (int i = 0; i <= arr.length - 2; i++) {
+
+			for (int j = 0; j <= arr.length - 2 - i; j++) {
+
+				if (arr[j] > arr[j + 1]) {
+					int temp = arr[j];
+					arr[j] = arr[j + 1];
+					arr[j + 1] = temp;
+				}
+
+			}
+
+		}
+
+	}
+
+}
