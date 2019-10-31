5e15b8f22198eecb2e60510854b6132dc2c44155
frankenstein32 <saifirashid32@gmail.com>
2019-10-31 20:13:10 +0530

    Lec22 Codes

--- src/Lec22/BTclient.java
+++ src/Lec22/BTclient.java
@@ -5,8 +5,23 @@ public class BTclient {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		BinaryTree bt = new BinaryTree();
+		String str = "10 true 20 true 40 false false true 50 false " + "false true 30 false true 60 false false";
+		BinaryTree bt = new BinaryTree(str);
 		bt.display();
+		System.out.println("Size: " + bt.size());
+		System.out.println("height: " + bt.height());
+		System.out.println("max: " + bt.max());
+		System.out.println("diamter1: " + bt.diameter());
+		System.out.println("diameter2 : " + bt.diameter2());
+
+		System.out.print("PreOrder: ");
+		bt.preorder();
+		System.out.println();
+		System.out.print("PostOrder: ");
+		bt.postorder();
+		System.out.println();
+		System.out.print("InOrder: ");
+		bt.inorder();
 	}
 
 }
--- src/Lec22/BinaryTree.java
+++ src/Lec22/BinaryTree.java
@@ -11,10 +11,11 @@ public class BinaryTree {
 	}
 
 	private Node root;
-	Scanner scn = new Scanner(System.in);
+	Scanner scn;
 
-	public BinaryTree() {
+	public BinaryTree(String str) {
 
+		scn = new Scanner(str);
 		root = construct(null, false);
 	}
 
@@ -24,25 +25,18 @@ public class BinaryTree {
 		int item = scn.nextInt();
 		nn.data = item;
 
-		System.out.println("Coming through" + nn.data);
-
 		boolean lc = scn.nextBoolean();
 
 		if (lc) {
 			nn.left = construct(nn, true);
-
-			System.out.println("Coming via" + nn.left.data);
 		}
 
-		System.out.println("Going through" + nn.data);
 		boolean rc = scn.nextBoolean();
 
 		if (rc) {
 			nn.right = construct(nn, false);
-			System.out.println("Coming via" + nn.right.data);
 		}
 
-		System.out.println("Bye" + nn.data);
 		return nn;
 	}
 
@@ -77,6 +71,228 @@ public class BinaryTree {
 
 		display(node.left);
 		display(node.right);
+
+	}
+
+	public int size() {
+		return size(root);
+	}
+
+	public int size(Node node) {
+
+		if (node == null) {
+			return 0;
+		}
+
+		int ls = size(node.left);
+		int rs = size(node.right);
+
+		return ls + rs + 1;
+
+	}
+
+	public int max() {
+		return max(root);
+	}
+
+	public int max(Node node) {
+
+		if (node == null) {
+			return Integer.MIN_VALUE;
+		}
+
+		int lm = max(node.left);
+		int rm = max(node.right);
+
+		return Math.max(node.data, Math.max(lm, rm));
+
+	}
+
+	public boolean find(int item) {
+		return find(root, item);
+	}
+
+	public boolean find(Node node, int item) {
+
+		if (node == null) {
+			return false;
+		}
+
+		if (node.data == item) {
+			return true;
+		}
+
+		boolean lans = find(node.left, item);
+		boolean rans = find(node.right, item);
+
+		return lans || rans;
+	}
+
+	public int height() {
+		return height(root);
+	}
+
+	public int height(Node node) {
+
+		if (node == null) {
+			return -1;
+		}
+
+		int lh = height(node.left);
+		int rh = height(node.right);
+
+		return Math.max(lh, rh) + 1;
+
+	}
+
+	public int diameter() {
+
+		return diameter(root);
+
+	}
+
+	public int diameter(Node node) {
+
+		if (node == null) {
+			return 0;
+		}
+
+		int lans = diameter(node.left);
+		int rans = diameter(node.right);
+
+		int self = height(node.left) + height(node.right) + 2;
+
+		return Math.max(self, Math.max(lans, rans));
+
+	}
+
+	public int diameter2() {
+		return diameter2(root).d;
+	}
+
+	class diaPair {
+
+		int d = 0;
+		int ht = -1;
+	}
+
+	public diaPair diameter2(Node node) {
+
+		if (node == null) {
+
+			return new diaPair();
+		}
+
+		diaPair lpair = diameter2(node.left);
+		diaPair rpair = diameter2(node.right);
+
+		diaPair spair = new diaPair();
+		spair.ht = Math.max(lpair.ht, rpair.ht) + 1;
+
+		int self = lpair.ht + rpair.ht + 2;
+
+		spair.d = Math.max(self, Math.max(lpair.d, rpair.d));
+
+		return spair;
+
+	}
+
+	public boolean isBalanced(Node node) {
+
+		if (node == null) {
+			return true;
+		}
+
+		boolean isL = isBalanced(node.left);
+		boolean isR = isBalanced(node.right);
+
+		int bf = height(node.left) - height(node.right);
+
+		return isL && isR && (bf == 0 || bf == -1 || bf == 1);
+
+	}
+
+	class balPair {
+
+		boolean isb = true;
+		int ht = -1;
+	}
+
+	public balPair isBalanced2(Node node) {
+
+		if (node == null) {
+			return new balPair();
+		}
+
+		balPair lp = isBalanced2(node.left);
+		balPair rp = isBalanced2(node.right);
+
+		balPair sp = new balPair();
+
+		sp.ht = Math.max(lp.ht, rp.ht) + 1;
+
+		int bf = lp.ht - rp.ht;
+
+		sp.isb = lp.isb && rp.isb && (bf == 0 || bf == -1 || bf == 1);
+
+		return sp;
+	}
+
+	public void preorder() {
+		preorder(root);
+	}
+
+	private void preorder(Node node) {
+
+		if (node == null) {
+			return;
+		}
+
+		// self work
+		System.out.print(node.data + " ");
+
+		// smaller problem
+		preorder(node.left);
+		preorder(node.right);
+
+	}
+
+	public void postorder() {
+		postorder(root);
+	}
+
+	private void postorder(Node node) {
+
+		if (node == null) {
+			return;
+		}
+
+		// smaller problem
+		postorder(node.left);
+		postorder(node.right);
+
+		// self work
+		System.out.print(node.data + " ");
+
+	}
+
+	public void inorder() {
+		inorder(root);
+	}
+
+	private void inorder(Node node) {
+
+		if (node == null) {
+			return;
+		}
+
+		// smaller problem
+		inorder(node.left);
+
+		// self work
+		System.out.print(node.data + " ");
+
+		inorder(node.right);
 
 	}
 
