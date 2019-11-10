0208ded492f05b045989ef8c97bc0aa709391e83
frankenstein32 <saifirashid32@gmail.com>
2019-11-10 15:55:45 +0530

    Lec29 Codes

--- src/Lec28/DP.java
+++ src/Lec28/DP.java
@@ -1,5 +1,7 @@
 package Lec28;
 
+import java.util.Arrays;
+
 public class DP {
 
 	public static void main(String[] args) {
@@ -9,12 +11,30 @@ public class DP {
 //		System.out.println(fiboBU(6));
 //		System.out.println(fiboBUSE(6));
 
-		System.out.println(boardPath(0, 10));
-		System.out.println(boardPathTD(0, 10, new int[11]));
-		System.out.println(boardPathBU(0, 10));
-		System.out.println(boardPathBUSE(0, 10));
+//		System.out.println(boardPath(0, 10));
+//		System.out.println(boardPathTD(0, 10, new int[11]));
+//		System.out.println(boardPathBU(0, 10));
+//		System.out.println(boardPathBUSE(0, 10));
 
 //		System.out.println(fibonacci2(100, new int[101]));
+
+		String s1 = "hfksjjkdshfkjsdhkjfhsdkjfdhfkjsdhfksdjfhkdsjabcd";
+		String s2 = "lkdjfd,fnsjdkfhkjsdhfkjksjfkdjfkdjdkjfdkjafd";
+//		System.out.println(LCS(s1, s2));
+
+		int[][] strg = new int[s1.length() + 1][s2.length() + 1];
+
+		for (int[] s : strg) {
+
+			Arrays.fill(s, -1);
+
+		}
+//		System.out.println(LCSTD(s1, s2, strg));
+//		System.out.println(LCSBU(s1, s2));
+
+//		System.out.println(EditDistance(s1, s2));
+		System.out.println(EditDistanceTD(s1, s2, strg));
+		System.out.println(EditDistanceBU(s1, s2));
 
 	}
 
@@ -212,6 +232,249 @@ public class DP {
 				}
 
 				strg[row][col] = strg[row + 1][col] + strg[row][col + 1];
+
+			}
+		}
+
+		return strg[0][0];
+
+	}
+
+	public static int mazePathBUSE(int er, int ec) {
+
+		int[] strg = new int[ec + 2];
+
+		for (int row = er; row >= 0; row--) {
+			for (int col = ec; col >= 0; col--) {
+
+				if (row == er && col == ec) {
+					strg[col] = 1;
+					continue;
+				}
+
+				strg[col] = strg[col] + strg[col + 1];
+
+			}
+		}
+
+		return strg[0];
+
+	}
+
+	public static int mazePathBUSE_Dia(int er, int ec) {
+
+		int[] strg = new int[ec + 2];
+
+		for (int row = er; row >= 0; row--) {
+			int dia = 0;
+			for (int col = ec; col >= 0; col--) {
+
+				if (row == er && col == ec) {
+					strg[col] = 1;
+					continue;
+				}
+
+				int temp = strg[col] + strg[col + 1] + dia;
+				dia = strg[col];
+				strg[col] = temp;
+
+			}
+		}
+
+		return strg[0];
+
+	}
+
+	public static int LCS(String s1, String s2) {
+
+		int l1 = s1.length();
+		int l2 = s2.length();
+
+		if (l1 == 0 || l2 == 0) {
+			return 0;
+		}
+
+		char ch1 = s1.charAt(0);
+		char ch2 = s2.charAt(0);
+
+		String ros1 = s1.substring(1);
+		String ros2 = s2.substring(1);
+
+		int ans = 0;
+		if (ch1 == ch2) {
+
+			ans = LCS(ros1, ros2) + 1;
+		} else {
+
+			int fp = LCS(s1, ros2);
+			int sp = LCS(ros1, s2);
+
+			ans = Math.max(fp, sp);
+		}
+
+		return ans;
+
+	}
+
+	public static int LCSTD(String s1, String s2, int[][] strg) {
+
+		int l1 = s1.length();
+		int l2 = s2.length();
+
+		if (l1 == 0 || l2 == 0) {
+			return 0;
+		}
+
+		if (strg[l1][l2] != -1) {
+			return strg[l1][l2];
+		}
+
+		char ch1 = s1.charAt(0);
+		char ch2 = s2.charAt(0);
+
+		String ros1 = s1.substring(1);
+		String ros2 = s2.substring(1);
+
+		int ans = 0;
+		if (ch1 == ch2) {
+
+			ans = LCSTD(ros1, ros2, strg) + 1;
+		} else {
+
+			int fp = LCSTD(s1, ros2, strg);
+			int sp = LCSTD(ros1, s2, strg);
+
+			ans = Math.max(fp, sp);
+		}
+
+		return strg[l1][l2] = ans;
+
+	}
+
+	public static int LCSBU(String s1, String s2) {
+
+		int[][] strg = new int[s1.length() + 1][s2.length() + 1];
+		for (int row = s1.length(); row >= 0; row--) {
+			for (int col = s2.length(); col >= 0; col--) {
+
+				if (row == s1.length() || col == s2.length()) {
+					strg[row][col] = 0;
+					continue;
+				}
+				char ch1 = s1.charAt(row);
+				char ch2 = s2.charAt(col);
+				int ans = 0;
+				if (ch1 == ch2) {
+
+					ans = strg[row + 1][col + 1] + 1;
+				} else {
+					ans = Math.max(strg[row + 1][col], strg[row][col + 1]);
+				}
+				strg[row][col] = ans;
+			}
+
+		}
+
+		return strg[0][0];
+	}
+
+	public static int EditDistance(String s1, String s2) {
+
+		if (s1.length() == 0 || s2.length() == 0) {
+			return Math.max(s1.length(), s2.length());
+		}
+
+		char ch1 = s1.charAt(0);
+		char ch2 = s2.charAt(0);
+
+		String ros1 = s1.substring(1);
+		String ros2 = s2.substring(1);
+
+		int ans = 0;
+
+		if (ch1 == ch2) {
+
+			ans = EditDistance(ros1, ros2);
+		} else {
+
+			int i = EditDistance(s1, ros2);
+			int r = EditDistance(ros1, ros2);
+			int d = EditDistance(ros1, s2);
+
+			ans = Math.min(i, Math.min(r, d)) + 1;
+		}
+
+		return ans;
+
+	}
+
+	public static int EditDistanceTD(String s1, String s2, int[][] strg) {
+
+		if (s1.length() == 0 || s2.length() == 0) {
+			return Math.max(s1.length(), s2.length());
+		}
+
+		if (strg[s1.length()][s2.length()] != -1) {
+			return strg[s1.length()][s2.length()];
+		}
+
+		char ch1 = s1.charAt(0);
+		char ch2 = s2.charAt(0);
+
+		String ros1 = s1.substring(1);
+		String ros2 = s2.substring(1);
+
+		int ans = 0;
+
+		if (ch1 == ch2) {
+
+			ans = EditDistanceTD(ros1, ros2, strg);
+		} else {
+
+			int i = EditDistanceTD(s1, ros2, strg);
+			int r = EditDistanceTD(ros1, ros2, strg);
+			int d = EditDistanceTD(ros1, s2, strg);
+
+			ans = Math.min(i, Math.min(r, d)) + 1;
+		}
+
+		return strg[s1.length()][s2.length()] = ans;
+
+	}
+
+	public static int EditDistanceBU(String s1, String s2) {
+
+		int[][] strg = new int[s1.length() + 1][s2.length() + 1];
+
+		for (int row = s1.length(); row >= 0; row--) {
+			for (int col = s2.length(); col >= 0; col--) {
+
+				if (row == s1.length()) {
+					strg[row][col] = s2.length() - col;
+					continue;
+				}
+
+				if (col == s2.length()) {
+					strg[row][col] = s1.length() - row;
+					continue;
+				}
+
+				char ch1 = s1.charAt(row);
+				char ch2 = s2.charAt(col);
+
+				int ans = 0;
+				if (ch1 == ch2) {
+					ans = strg[row + 1][col + 1];
+				} else {
+
+					int i = strg[row][col + 1];
+					int d = strg[row + 1][col];
+					int r = strg[row + 1][col + 1];
+
+					ans = Math.min(i, Math.min(d, r)) + 1;
+				}
+
+				strg[row][col] = ans;
 
 			}
 		}
